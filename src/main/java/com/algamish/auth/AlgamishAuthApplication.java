e831bd05fd373e57d29edc8ee6ab007a4b0d74ef
Lucas Barbosa <lucas.lopesifrn@gmail.com>
2021-04-01 17:29:24 -0300

    Initial commit

---
+++ src/main/java/com/algamish/auth/AlgamishAuthApplication.java
@@ -0,0 +1,13 @@
+package com.algamish.auth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlgamishAuthApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AlgamishAuthApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/algamish/auth/AlgamishAuthApplicationTests.java
@@ -0,0 +1,13 @@
+package com.algamish.auth;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AlgamishAuthApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
